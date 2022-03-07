b86a37f5865a34a7aa25b9e9ee15001c6ab82be3
dialfonsop <73137772+dialfonsop@users.noreply.github.com>
2022-03-07 16:57:14 -0500

    .
    commit

--- ws/src/routes/servicio.routes.js
+++ ws/src/routes/servicio.routes.js
@@ -8,14 +8,15 @@ const Servicio = require("../models/servicio.js");
 const { json } = require("express");
 
 router.post("/", async (req, res) => {
-  let busboy = new Busboy({ headers: req.header });
+  let busboy = new Busboy({ headers: req.headers });
   busboy.on("finish", async () => {
     try {
       const { salaId, servicio } = req.body;
       let errores = [];
       let archivos = [];
 
-      if (req.files && Object.keys(req.files) > 0) {
+
+      if (req.files && Object.keys(req.files).length > 0) {
         for (let key of Object.keys(req.files)) {
           const file = req.files[key];
 
@@ -28,39 +29,92 @@ router.post("/", async (req, res) => {
           const response = await aws.uploadToS3(file, path);
 
           if (response.error) {
-            errores.push({ error: true, message: response.message.message });
+            errores.push({ error: true, message: response.message });
           } else {
             archivos.push(path);
           }
         }
-
-        if (errores.length > 0) {
-          res.json(errores[0]);
-          return false;
-        }
-
-        //Crear servicio
-        let jsonServicio = JSON.parse(servicio);
-        const servicioRegistro = await Servicio(jsonServicio).save();
-
-        // Crear Archivo
-        archivos = archivos.map((archivo) => ({
-          referenciaId: servicioRegistro._id,
-          model: "Servicio",
-          camino: archivo,
-        }));
-
-        await Archivo.insertMany(archivos);
-
-        res.json({ servicio: servicioRegistro, archivos })
-
-
       }
+
+      if (errores.length > 0) {
+        res.json(errores[0]);
+        return false;
+      }
+
+      //Crear servicio
+      let jsonServicio = JSON.parse(servicio);
+      const servicioRegistro = await Servicio(jsonServicio).save();
+
+      // Crear Archivo
+      archivos = archivos.map((archivo) => ({
+        referenciaId: servicioRegistro._id,
+        model: "Servicio",
+        camino: archivo,
+      }));
+
+      await Archivo.insertMany(archivos);
+
+      res.json({ servicio: servicioRegistro, archivos });
     } catch (error) {
       res.json({ error: true, message: error.message });
     }
   });
-  req.pipe();
+  req.pipe(busboy);
+});
+
+router.put("/:id", async (req, res) => {
+  let busboy = new Busboy({ headers: req.headers });
+  busboy.on("finish", async () => {
+    try {
+      const { salaId, servicio } = req.body;
+      let errores = [];
+      let archivos = [];
+
+
+      if (req.files && Object.keys(req.files).length > 0) {
+        for (let key of Object.keys(req.files)) {
+          const file = req.files[key];
+
+          const nameParts = file.name.split(".");
+          const fileName = `${new Date().getTime()}.${
+            nameParts[nameParts.length - 1]
+          }`;
+          const path = `servicios/${salaId}/${fileName}`;
+
+          const response = await aws.uploadToS3(file, path);
+
+          if (response.error) {
+            errores.push({ error: true, message: response.message });
+          } else {
+            archivos.push(path);
+          }
+        }
+      }
+
+      if (errores.length > 0) {
+        res.json(errores[0]);
+        return false;
+      }
+
+      //Crear servicio
+      const jsonServicio = JSON.parse(servicio);
+      await Servicio.findByIdAndUpdate(req.params.id, jsonServicio);
+
+      // Crear Archivo
+      archivos = archivos.map((archivo) => ({
+        referenciaId: req.params.id,
+        model: "Servicio",
+        camino: archivo,
+      }));
+
+      await Archivo.insertMany(archivos);
+
+      res.json({ error: false });
+    } catch (error) {
+      res.json({ error: true, message: error.message });
+    }
+  });
+  req.pipe(busboy);
 });
 
 module.exports = router;
